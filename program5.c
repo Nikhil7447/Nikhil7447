6a4dd8b2d0d4677c3f5a6d48256ebcb6e5502986
Nikhil7447 <106022197+Nikhil7447@users.noreply.github.com>
2022-05-28 19:18:44 +0530

    Update program5.c

--- program5.c
+++ program5.c
@@ -5,24 +5,14 @@
 	int iAns = 0;
 	iAns = iValue1 + iValue2;
 	return iAns;
-
-
 }	
-	int Subtraction(int iValue1,int iValue2)
-{
-	int iAns = 0;
-	iAns = iValue1 - iValue2;
-	return iAns;
-}
-
+	
 int main()
 {
 	int iValue1=0;
 	int iValue2=0;
 	int iValue3=0;
-	int iValue4=0;
-	
-	
+
 	
 	printf("Enter first number\n");
 	scanf("%d",&iValue1);
@@ -31,13 +21,9 @@ int main()
 	scanf("%d",&iValue2);
 	
 	iValue3=Addition(iValue1,iValue2);
-
-	iValue4=Subtraction(iValue1,iValue2);
-
 	
 	printf("Addition is : %d\n",iValue3);
-	printf("Subtraction is : %d\n",iValue4);
-
+	
 return 0;
 }
 
